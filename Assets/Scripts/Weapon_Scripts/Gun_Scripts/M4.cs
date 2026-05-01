32364f44759e92acf3b03d8d39182545ca580679
Tommy <89096637+TCipresso@users.noreply.github.com>
2026-05-01 13:41:13 -0500

    Update 123
    Added m4 with red dot sight

---
+++ Assets/Scripts/Weapon_Scripts/Gun_Scripts/M4.cs
@@ -0,0 +1,79 @@
+using UnityEngine;
+
+public class M4 : WeaponBase
+{
+    [Header("M4 Settings")]
+    public float range = 50f;
+    public int damagePerBullet = 35;
+
+    float nextFireTime = 0f;
+
+    protected override void Awake()
+    {
+        base.Awake();
+        currentMag = maxMag;
+    }
+
+    public override void Shoot()
+    {
+        if (!CanShoot()) return;
+        if (Time.time < nextFireTime) return;
+        nextFireTime = Time.time + FireInterval;
+
+        currentMag--;
+
+        TriggerFireAnimation();
+        PlayFireSound();
+        PlayMuzzleFlash();
+        ApplyRecoil();
+        AddBloom();
+        FireBullet();
+
+        if (currentMag <= 0 && reserveAmmo > 0)
+            Reload();
+    }
+
+    void FireBullet()
+    {
+        Vector3 direction = GetAimDirection(0f, 0f);
+        Vector3 origin = GetAimOrigin();
+        Ray ray = new Ray(origin, direction);
+        Vector3 endPoint;
+
+        if (Physics.Raycast(ray, out RaycastHit hit, range))
+        {
+            endPoint = hit.point;
+
+            HitBox hitBox = hit.collider.GetComponent<HitBox>();
+            if (hitBox != null)
+            {
+                hitBox.TakeDamageWithHitPoint(damagePerBullet, playerStats, hit.point, playerStats != null ? playerStats.goldGainMultiplier : 1f);
+            }
+            else
+            {
+                ZombieBase zombie = hit.collider.GetComponent<ZombieBase>();
+                if (zombie != null)
+                {
+                    zombie.TakeDamage(ApplyCrit(damagePerBullet), playerStats, playerStats != null ? playerStats.goldGainMultiplier : 1f);
+                    if (HitMarkerPool.Instance != null)
+                        HitMarkerPool.Instance.Spawn(hit.point, false);
+                }
+            }
+
+            SpawnImpactEffect(hit);
+        }
+        else
+        {
+            endPoint = origin + direction * range;
+        }
+
+        SpawnTrail(muzzlePoint.position, endPoint);
+    }
+
+    public override void Reload()
+    {
+        if (isReloading || currentMag == maxMag || reserveAmmo <= 0) return;
+        TriggerReloadAnimation();
+        Debug.Log("[M4] Reloading...");
+    }
+}
