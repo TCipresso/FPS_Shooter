0ef9635eabfea544eb6ec56781fe31370323d4a0
Tommy <89096637+TCipresso@users.noreply.github.com>
2026-05-31 18:21:02 -0500

    UDpate 192 Added the deag and glicky

---
+++ Assets/Scripts/Weapon_Scripts/Weapon_Packages/Deag/Deag.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+public class Deag : WeaponBase
+{
+    float nextFireTime = 0f;
+    protected override void Awake()
+    {
+        base.Awake();
+        currentMag = maxMag;
+    }
+    public override void Shoot()
+    {
+        if (!CanShoot()) return;
+        if (Time.time < nextFireTime) return;
+        nextFireTime = Time.time + FireInterval;
+        currentMag--;
+        TriggerFireAnimation();
+        PlayFireSound();
+        PlayMuzzleFlash();
+        ApplyRecoil();
+        AddBloom();
+        AddBloom();
+        Fire(damage);
+        if (currentMag <= 0 && reserveAmmo > 0)
+            Reload();
+    }
+    public override void Reload()
+    {
+        if (isReloading || currentMag == maxMag || reserveAmmo <= 0) return;
+        TriggerReloadAnimation();
+        Debug.Log("[Deag] Reloading...");
+    }
+}
