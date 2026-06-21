69e17b8cf88ea70ac879dd8b914a57ecea7fc38d
Tommy <89096637+TCipresso@users.noreply.github.com>
2026-06-20 20:48:34 -0500

    Udpate 238
    Added LMG

---
+++ Assets/Scripts/Weapon_Scripts/Weapon_Packages/LMG/LMG.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+public class LMG : WeaponBase
+{
+    float nextFireTime = 0f;
+    protected override void Awake()
+    {
+        base.Awake();
+        currentMag = maxMag;
+    }
+    public override void Shoot()
+    {
+        if (!CanShoot()) return;
+        if (Time.time < nextFireTime) return;
+        nextFireTime = Time.time + FireInterval;
+        currentMag--;
+        TriggerFireAnimation();
+        PlayFireSound();
+        PlayMuzzleFlash();
+        ApplyRecoil();
+        AddBloom();
+        Fire(damage);
+        if (currentMag <= 0 && reserveAmmo > 0)
+            Reload();
+    }
+    public override void Reload()
+    {
+        if (isReloading || currentMag == maxMag || reserveAmmo <= 0) return;
+        TriggerReloadAnimation();
+        Debug.Log("[LMG] Reloading...");
+    }
+}
