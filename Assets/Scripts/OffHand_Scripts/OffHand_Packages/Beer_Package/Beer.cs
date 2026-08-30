e43142e5fc789e00f480a0469786f2d2622b9f14
Tommy <89096637+TCipresso@users.noreply.github.com>
2026-08-30 12:52:12 -0500

    Slop 31 Beer is good to fo

---
+++ Assets/Scripts/OffHand_Scripts/OffHand_Packages/Beer_Package/Beer.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class Beer : WeaponBase
+{
+    float nextFireTime = 0f;
+
+    public override void Shoot()
+    {
+        if (!CanShoot()) return;
+        if (Time.time < nextFireTime) return;
+        nextFireTime = Time.time + FireInterval;
+
+        TriggerFireAnimation();
+        PlayFireSound();
+        PlayMuzzleFlash();
+        ApplyRecoil();
+        AddBloom();
+        AddBloom();
+        Fire(damage);
+    }
+}
